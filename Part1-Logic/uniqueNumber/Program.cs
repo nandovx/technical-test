64990253ab3c5208bec0e5fc51ac806e8ed99ac2
Fernando <fernando.batista@jala.university>
2025-08-11 13:24:41 -0300

    feat: logic test, readme and gitignore

---
+++ Part1-Logic/uniqueNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine(
+                "Please run with a argument like" +
+            " 'dotnet run -- {your number list}'" +
+            " if you have any doubts read the README");
+            return;
+        }
+
+        List<int> numList = Preprocess(args[0]);
+        UniqueNumberFinder(numList);
+    }
+
+    static List<int> Preprocess(string strList)
+    {
+        List<int> nums = strList
+            .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Trim())
+            .Where(s => !string.IsNullOrEmpty(s))
+            .Select(s => int.Parse(s))
+            .ToList();
+
+        nums.Sort();
+
+        return nums;
+    }
+
+    static void UniqueNumberFinder(List<int> numList)
+    {
+        HashSet<int> uniqueNumbers = new HashSet<int>(numList);
+        for (int i = 1; i < numList.Count; i++)
+        {
+            if (numList[i] == numList[i - 1])
+            {
+                uniqueNumbers.Remove(numList[i]);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", uniqueNumbers));
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # technical-test
-A technical test to prove my hard and softskills to a enterprise 
+
+A technical test to demonstrate both hard and soft skills for a company evaluation.
+
+Cloning the repository
+
+1.1 ssh
+
+```bash
+git clone git@github.com:nandovx/technical-test.git
+```
+
+1.2 https
+
+```bash
+git clone https://github.com/nandovx/technical-test.git
+```
+
+## Part 1 - Logic
+
+### 🛠 Requirements
+
+- .NET SDK 8.0+ installed
+
+  Check installation with:
+
+  ```bash
+  dotnet --version
+  ```
+
+### 🚀 How to Run
+
+1. Navigate to the project folder
+
+   ```bash
+   cd technical-test
+   cd Part1-Logic/uniqueNumber
+   ```
+
+2. Run the program
+
+   ```bash
+   dotnet run -- "1,2,3,3,4,4,5,6"
+   ```
